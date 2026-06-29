4c6069ec4940a8f7876748712dcd5b0535dd0545
mkt501881-art <mkt501881@gmail.com>
2026-06-29 17:53:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -21,4 +22,6 @@ def update():
 
     return "ok"
 
-app.run()
+port = int(os.environ.get("PORT", 5000))
+
+app.run(host="0.0.0.0", port=port)
